48a990511f0d375ceef74cba3b135aec23323b21
Jose Gonzalez <joegonzo990@gmail.com>
2018-03-01 21:04:29 -0600

    Completed assessment

--- assessment.js
+++ assessment.js
@@ -7,69 +7,104 @@ After writing your function uncomment the matching function reference at the bot
 // 1. Write a function called helloWorld that returns the string 'Hello World!'.
 
 function helloWorld() {
-
-}
-
-/*
-2. Write a function called lambdaSchool that has a single parameter called num.
-     num will be a positive integer.
-     If num is divisible by 3 return the string 'Lambda'
-     If num is divisible by 5 return the string 'School'
-     If num is divisible by 3 AND 5 return the string 'Lambda School' (notice the space)
-     If num is NOT divisible by 3 or 5 then return num.
-
-     Example:
-             lambdaSchool(15); // returns 'Lambda School'
-             lambdaSchool(8); // returns 8
-*/
-
-function lambdaSchool() {
-
-}
-
-/*
-3. Write a function called longestString that has a single parameter called strs.
-     strs will be an array of strings.
-     Return the longest string that is in strs.
-     If there is a tie for the longest string then return the one that occurs first.
-
-     Example:
-             longestString(['hi', 'hello', 'ni hao', 'guten tag']); // returns 'guten tag'
-             longestString(['JavaScript', 'HTML', 'CSS']); // returns 'JavaScript'
-*/
-
-function longestString() {
-
-}
-
-/*
-4. Write a function called computeUserAverageAge that has a single parameter called users
-     users is an array of user objects.
-     Each user object has a property called age that is a number.
-     Compute the average age of all user objects in the users array.
-     Round off the decimals if needed and return the number.
-
-     Example:
-             const users = [{
-               name: 'Brendan Eich',
-               age: 56,
-             }, {
-               name: 'Linus Torvalds',
-               age: 48,
-             }, {
-               name: 'Margaret Hamilton',
-               age: 81,
-             }];
-             computeUserAverageAge(users); // returns 62 (This number is rounded up from 61.6666)
-*/
-
-function computeUserAverageAge() {
-
-}
-
-module.exports = {
- helloWorld,
- lambdaSchool,
- longestString,
- computeUserAverageAge
-};
+  
+  return 'Hello World!';
+  }
+  
+  /*
+  2. Write a function called lambdaSchool that has a single parameter called num.
+       num will be a positive integer.
+       If num is divisible by 3 return the string 'Lambda'
+       If num is divisible by 5 return the string 'School'
+       If num is divisible by 3 AND 5 return the string 'Lambda School' (notice the space)
+       If num is NOT divisible by 3 or 5 then return num.
+  
+       Example:
+               lambdaSchool(15); // returns 'Lambda School'
+               lambdaSchool(8); // returns 8
+  */
+  
+  function lambdaSchool(num) {
+  
+    var num;
+    if ((((num % 3)== 0)&&((num % 5 )==0))){
+      return "Lambda School";
+    }else if ((((  num % 3)!= 0)&&((num % 5 )!=0))){
+      return num;
+    }else if((num % 3)==0){
+      return "Lambda";
+    } else if ((num % 5) ==0){
+      return "School";
+    }
+  }
+  
+  /*
+  3. Write a function called longestString that has a single parameter called strs.
+       strs will be an array of strings.
+       Return the longest string that is in strs.
+       If there is a tie for the longest string then return the one that occurs first.
+  
+       Example:
+               longestString(['hi', 'hello', 'ni hao', 'guten tag']); // returns 'guten tag'
+               longestString(['JavaScript', 'HTML', 'CSS']); // returns 'JavaScript'
+  */
+  
+  function longestString(strs) {
+    
+    var strs;
+    
+    var longestlength = 0;
+    var longestString;
+    for (var counter = 0; counter < strs.length; counter++){
+      
+      if(strs[counter].length > longestlength)
+      {
+      longestlength = strs[counter].length;
+      longestString = strs[counter];
+      }
+  }
+    return longestString;
+  }
+  
+  /*
+  4. Write a function called computeUserAverageAge that has a single parameter called users
+       users is an array of user objects.
+       Each user object has a property called age that is a number.
+       Compute the average age of all user objects in the users array.
+       Round off the decimals if needed and return the number.
+  
+       Example:
+               const users = [{
+                 name: 'Brendan Eich',
+                 age: 56,
+               }, {
+                 name: 'Linus Torvalds',
+                 age: 48,
+               }, {
+                 name: 'Margaret Hamilton',
+                 age: 81,
+               }];
+               computeUserAverageAge(users); // returns 62 (This number is rounded up from 61.6666)
+  */
+  
+  function computeUserAverageAge(users) {
+    
+  var totalAges = 0;
+  var average = 0;
+   for(var counter = 0; counter < users.length; counter++)
+      totalAges = totalAges + users[counter].age;
+    
+    
+    
+    average = totalAges/users.length;
+    return Math.round(average);
+  
+  
+  }
+  
+  module.exports = {
+   helloWorld,
+   lambdaSchool,
+   longestString,
+   computeUserAverageAge
+  };
